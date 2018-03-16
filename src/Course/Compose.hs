21ebf2e329bf013f729d4edd0acaf8ba175d6e83
Daniel Brice <danielbrice@gmail.com>
2018-03-16 13:19:15 -0700

    finished Course.Compose, starting Course.Traversable

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -10,26 +10,14 @@ import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
--- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  f <$> (Compose xs) = Compose ((f <$>) <$> xs)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure x = Compose (pure (pure x))
+  (Compose fs) <*> (Compose xs) = Compose (lift2 (<*>) fs xs)
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+instance (Monad f, Monad g) => Monad (Compose f g) where
+  (=<<) = error "this one is not possible"
